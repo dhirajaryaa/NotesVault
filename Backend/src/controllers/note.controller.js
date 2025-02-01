925f3f605a48efc30261fff867c2cbf7e2f53a14
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-02-01 19:19:05 +0530

    feat: Add nanoid for generating shared links in notes creation

--- Backend/src/controllers/note.controller.js
+++ Backend/src/controllers/note.controller.js
@@ -2,12 +2,13 @@ import { AsyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/apiError.js";
 import { Note } from "../../models/notes.model.js";
+import {nanoid} from "nanoid";
 
 // create new notes
 const createNotes = AsyncHandler(async (req, res) => {
-  const { title, password, content, isSecure, tags } = req.body;
+  const { title, password, content, isSecure, tags, sharedLink } = req.body;
 
-  if ([title, password, content, isSecure, tags].some((item) => item === "")) {
+  if ([title, password, content, isSecure].some((item) => item === "")) {
     throw new ApiError(400, "All fields are required");
   }
 
@@ -16,11 +17,15 @@ const createNotes = AsyncHandler(async (req, res) => {
     content,
     password,
     isSecure,
-    tags,
+    tags: tags || [],
+    sharedLink: sharedLink || nanoid(10),
   });
-  
 
-  res.status(201).json(new ApiResponse(201,collectionRes ,"Notes Created :)"));
+  const data = await Note.findById(collectionRes._id).select(
+    "-password -viewCount"
+  );
+
+  res.status(201).json(new ApiResponse(201, data, "Notes Created :)"));
 });
 
 export { createNotes };
