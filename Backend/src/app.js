3dfc561d2f4323be1da3959d27bcd515675e3e2a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-02-01 09:13:15 +0530

    feat: Configure CORS and enhance Express app middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,15 @@
 import express from 'express';
+import cors from "cors"
 
 export const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: process.env.ALLOWED_ORIGIN
+}));
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
